9aa1711ac114e280287e61f53250f55a01d174b6
marcorigo <marcorigo99@gmail.com>
2019-09-16 20:32:14 +0200

    Basic Rendering Test :poop:

--- scripts/view.js
+++ scripts/view.js
@@ -1,20 +1,64 @@
-class Render {
-    constructor(canvas, world, width, height, scale = false) {
+class RenderEngine {
+    constructor(canvas, world, width, height, blocksize = false, scale = false) {
         this.canvas = canvas
         this.world = world
         this.width = width
         this.height = height
         this.scale = scale
-        this.ctx = this.canvas.getContext('2d')
+        this.blocksize = blocksize
+        this.worldMaxY = this.world.maxY
+        this.maxY = this.world.maxY
+        this.minX = this.world.minX
+        this.windowWidth = window.innerWidth
+        this.windowHeight = window.innerHeight
+        this.ctx = canvas.getContext('2d')
         this.scale && this.attachCanvasResizeEvent()
+        this.resizeCanvas(this.windowWidth, this.windowHeight)
+        if(!blocksize) {
+            this.blocksize = this.windowWidth / 10
+        }
     }
     attachCanvasResizeEvent() {
         window.addEventListener('resize', () => {
-            this.resizeCanvas(window.innerWidth, window.innerHeight)
+            this.windowWidth = window.innerWidth
+            this.windowHeight = window.innerHeight
+            this.resizeCanvas( this.windowWidth, this.windowHeight)
         })
     }
     resizeCanvas(width, height) {
         this.canvas.width = width
         this.canvas.height = height
     }
+    render(startX, startY, endX, endY) {
+        console.log(this.world.worldArray)
+        this.clearCanvas()
+        for(let y = startY; y < endY; y ++) {
+            for(let x = startX; x < endX; x ++) {
+                let block = this.world.worldArray[(this.worldMaxY * y) + x]
+                if(block != 0) {
+                    let color = this.blockManager(block)
+                    this.ctx.fillStyle = color;
+                    this.ctx.fillRect (x * this.blocksize ,y * this.blocksize , this.blocksize, this.blocksize)
+                }
+            }
+        }
+    }
+    clearCanvas(){
+        this.ctx.fillStyle = "lightblue";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    blockManager(blockId) {
+    if (blockId == 1)
+        return 'green'
+    else if (blockId == 2)
+        return 'brown'
+    else if (blockId == 3)
+        return 'gray'
+    else if (blockId == 4)
+        return '#0077be'
+    else if (blockId == 5)
+        return '#c2b280'
+    else
+        return 'purple'
+}
 }
