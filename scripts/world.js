c8a24c53f57ebdf55db3bafe9c3603205c4bb0a3
marcorigo <marcorigo99@gmail.com>
2019-09-16 20:29:40 +0200

    Changed generation flow :ambulance:

--- scripts/world.js
+++ scripts/world.js
@@ -28,23 +28,24 @@ class World {
     generate(startX, endX) {
         for(let i = startX; i < endX; i ++) {
             let colHeight = parseInt(this.noiseGen.getNoise(i, this.maxY - this.minY, this.chunkSize, this.noise))
+            let col = []
             for(let j = this.minY; j < this.maxY; j ++ ) {
-                let je = this.maxY - j
                 let blockId
-                if (je > colHeight && je < this.maxY / 3)
+                if (j > colHeight && j < this.maxY / 3)
                     blockId = 4
-                else if (je > colHeight)
+                else if (j > colHeight)
                     blockId = 0
-                else if (je == colHeight && je < this.maxY / 3 + 1)
+                else if (j == colHeight && j < this.maxY / 3 + 1)
                     blockId = 5
-                else if (je == colHeight)
+                else if (j == colHeight)
                     blockId = 1
-                else if (je > colHeight - 5)
+                else if (j > colHeight - 5)
                     blockId = 2
                 else
                     blockId = 3
-                this.saveWorld(blockId)
+                col.push(blockId)
             }
+            this.saveWorld(col)
         }
     }
 }
